1a7b19b18c91d6c9feece910fe0ce4af656f079b
PuuTzzA <putzerthomas55@gmail.com>
2022-12-28 18:25:39 +0100

    add sky dome

---
+++ Assets/Scripts/CenterMouse.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Grappling.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Grappling : MonoBehaviour
+{
+    // Based on:
+    // https://www.youtube.com/watch?v=TYzZsBl3OI0&ab_channel=Dave%2FGameDevelopment
+
+    [Header("References")] [SerializeField]
+    private Transform cam;
+
+    [SerializeField] private Transform gunTip;
+    [SerializeField] private LayerMask layerMaskGrapple;
+    [SerializeField] private LineRenderer lineRenderer;
+
+    [Header("Grappling")] [SerializeField] private float maxGrappleDistance;
+    [SerializeField] private float grappleDelayTime;
+    [SerializeField] private float grapplingCooldown;
+    [SerializeField] private float overshootYAxis;
+    
+    [Header("Input")] 
+    [SerializeField] private KeyCode grappleKey = KeyCode.Mouse0;
+
+    private Vector3 _grapplePoint;
+    private float _cooldownTimer;
+    private bool _grappling;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(grappleKey))
+        {
+            StartGrapple();
+        }
+
+        if (_cooldownTimer > 0)
+        {
+            _cooldownTimer -= Time.deltaTime;
+        }
+
+        // Debug.DrawLine(cam.position, cam.position + cam.forward * maxGrappleDistance, Color.black);
+    }
+
+    private void LateUpdate()
+    {
+        if (_grappling)
+        {
+            lineRenderer.SetPosition(0, gunTip.position);
+        }
+    }
+
+    private void StartGrapple()
+    {
+        if (_cooldownTimer > 0)
+        {
+            return;
+        }
+
+        _grappling = true;
+
+        if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hit, maxGrappleDistance, layerMaskGrapple))
+        {
+            _grapplePoint = hit.point;
+            
+            Invoke(nameof(ExecuteGrapple), grappleDelayTime);
+        }
+        else
+        {
+            _grapplePoint = cam.position + cam.forward * maxGrappleDistance;
+
+            Invoke(nameof(StopGrapple), grappleDelayTime);
+        }
+
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(1, _grapplePoint);
+    }
+
+    private void ExecuteGrapple()
+    {
+        Vector3 pos = transform.position;
+        Vector3 lowestPoint = new Vector3(pos.x, pos.y - 1f, pos.z);
+        float grapplePointRelativeYPos = _grapplePoint.y - lowestPoint.y;
+        float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
+        if (grapplePointRelativeYPos < 0)
+        {
+            highestPointOnArc = overshootYAxis;
+        }
+        
+        GetComponent<PlayerMovement>().JumpToPosition(_grapplePoint, highestPointOnArc);
+        
+        Invoke(nameof(StopGrapple), 1f);
+    }
+
+    public void StopGrapple()
+    {
+        _grappling = false;
+
+        _cooldownTimer = grapplingCooldown;
+
+        lineRenderer.enabled = false;
+    }
+}
