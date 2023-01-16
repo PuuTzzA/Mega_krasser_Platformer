23ba8ea328ac0f469cd76563f284aeab4de5cd2a
Gift69 <gift2lp4k@gmail.com>
2023-01-16 15:10:06 +0100

    adedd vertical camera follow and fixed rotational follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -12,13 +12,14 @@ public class CameraFollow : MonoBehaviour
     private GameObject tmp;
 
     [SerializeField]
-    float sensibility = 1f;
+    private float rotationSensibility = 1f;
+    [SerializeField]
+    private float verticalSensibility = 1f;
     // Start is called before the first frame update
     void Start()
     {
         tmp = new GameObject();
         _rotationPoint = new GameObject("RotationPoint");
-        _rotationPoint.transform.localScale = new Vector3(1, 1, 1);
         if (_pointOfReference == null)
         {
             _rotationPoint.transform.position = new Vector3(0, 0, 0);
@@ -29,7 +30,7 @@ public class CameraFollow : MonoBehaviour
         }
         this.transform.SetParent(_rotationPoint.transform);
         this.transform.eulerAngles = new Vector3(0, 180, 0);
-        this.transform.position = new Vector3(0, 0, 20);
+        this.transform.position = new Vector3(0, 0, 60);
     }
 
     // Update is called once per frame
@@ -37,13 +38,16 @@ public class CameraFollow : MonoBehaviour
     {
         if (player != null)
         {
-            
+
             tmp.transform.position = _rotationPoint.transform.position;
             tmp.transform.localScale = _rotationPoint.transform.localScale;
             tmp.transform.rotation = _rotationPoint.transform.rotation;
             tmp.transform.LookAt(new Vector3(player.transform.position.x - player.transform.localScale.x / 4, player.transform.position.y, player.transform.position.z - player.transform.localScale.z / 4));
-            _rotationPoint.transform.rotation = Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation,Time.deltaTime * sensibility * Mathf.Abs(_rotationPoint.transform.eulerAngles.y - tmp.transform.eulerAngles.y));
-            Debug.Log(_rotationPoint.transform.eulerAngles.ToString() + " " +  tmp.transform.eulerAngles.ToString());
+            tmp.transform.rotation = new Quaternion(0, tmp.transform.rotation.y, 0, tmp.transform.rotation.w);
+            //_rotationPoint.transform.rotation = Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation,Time.deltaTime * sensibility * Mathf.Abs(_rotationPoint.transform.eulerAngles.y - tmp.transform.eulerAngles.y));
+            _rotationPoint.transform.rotation = Quaternion.Slerp(_rotationPoint.transform.rotation, Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation, 360), Time.deltaTime * rotationSensibility);
+            Debug.Log(_rotationPoint.transform.eulerAngles.ToString() + " " + tmp.transform.eulerAngles.ToString());
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, player.transform.position.y, transform.position.z), Time.deltaTime * verticalSensibility);
 
         }
     }
