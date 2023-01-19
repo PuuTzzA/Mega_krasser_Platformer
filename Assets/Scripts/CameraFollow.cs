3c7cc239015bef6aac45f25085c908b21532fdd5
Gift69 <gift2lp4k@gmail.com>
2023-01-19 20:13:27 +0100

    für mulsi

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,6 +15,11 @@ public class CameraFollow : MonoBehaviour
     private float rotationSensibility = 1f;
     [SerializeField]
     private float verticalSensibility = 1f;
+
+    [SerializeField]
+    private float radius;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,25 +35,38 @@ public class CameraFollow : MonoBehaviour
         }
         this.transform.SetParent(_rotationPoint.transform);
         this.transform.eulerAngles = new Vector3(0, 180, 0);
-        this.transform.position = new Vector3(0, 0, 60);
+        this.transform.position = new Vector3(0, 0, radius);
+
+        tmp.transform.position = _rotationPoint.transform.position;
+        tmp.transform.localScale = _rotationPoint.transform.localScale;
+        tmp.transform.rotation = _rotationPoint.transform.rotation;
+
+        tmp.transform.LookAt(new Vector3(player.transform.position.x - player.transform.localScale.x / 4, player.transform.position.y, player.transform.position.z - player.transform.localScale.z / 4));
+        tmp.transform.rotation = new Quaternion(0, tmp.transform.rotation.y, 0, tmp.transform.rotation.w);
+
+        _rotationPoint.transform.rotation = Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation, 360);
     }
-
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        if (player != null)
-        {
 
-            tmp.transform.position = _rotationPoint.transform.position;
-            tmp.transform.localScale = _rotationPoint.transform.localScale;
-            tmp.transform.rotation = _rotationPoint.transform.rotation;
-            tmp.transform.LookAt(new Vector3(player.transform.position.x - player.transform.localScale.x / 4, player.transform.position.y, player.transform.position.z - player.transform.localScale.z / 4));
-            tmp.transform.rotation = new Quaternion(0, tmp.transform.rotation.y, 0, tmp.transform.rotation.w);
-            //_rotationPoint.transform.rotation = Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation,Time.deltaTime * sensibility * Mathf.Abs(_rotationPoint.transform.eulerAngles.y - tmp.transform.eulerAngles.y));
-            _rotationPoint.transform.rotation = Quaternion.Slerp(_rotationPoint.transform.rotation, Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation, 360), Time.deltaTime * rotationSensibility);
-            Debug.Log(_rotationPoint.transform.eulerAngles.ToString() + " " + tmp.transform.eulerAngles.ToString());
-            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, player.transform.position.y, transform.position.z), Time.deltaTime * verticalSensibility);
+        Vector3 relativePos = player.transform.position - _rotationPoint.transform.position;
+        relativePos = new Vector3(relativePos.x,0,relativePos.z);
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
 
-        }
+        Quaternion current = _rotationPoint.transform.localRotation;
+        Debug.Log(Time.deltaTime);
+        //Debug.Log(Quaternion.Angle(current, rotation) * Time.deltaTime * rotationSensibility);
+        _rotationPoint.transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime * rotationSensibility);
+
+
+        /* tmp.transform.position = _rotationPoint.transform.position;
+         tmp.transform.localScale = _rotationPoint.transform.localScale;
+         tmp.transform.rotation = _rotationPoint.transform.rotation;
+         tmp.transform.LookAt(new Vector3(player.transform.position.x - player.transform.localScale.x / 4, player.transform.position.y, player.transform.position.z - player.transform.localScale.z / 4));
+         tmp.transform.rotation = new Quaternion(0, tmp.transform.rotation.y, 0, tmp.transform.rotation.w);
+         _rotationPoint.transform.rotation = Quaternion.Slerp(_rotationPoint.transform.rotation, Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation, 360), Time.deltaTime * rotationSensibility);
+         */
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, player.transform.position.y, transform.position.z), Time.deltaTime * verticalSensibility);
     }
 }
