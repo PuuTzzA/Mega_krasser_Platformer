c8b6e48696fa9305220ab9bf559a58e78553a129
Gift69 <gift2lp4k@gmail.com>
2023-01-23 14:39:38 +0100

    some refactoring

--- Assets/Scripts/UIStart.cs
+++ Assets/Scripts/UIStart.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class UIPreview : MonoBehaviour
+public class UIStart : MonoBehaviour
 {
+
 
     [SerializeField]
     private GameObject[] levels;
@@ -12,15 +14,39 @@ public class UIPreview : MonoBehaviour
 
     private GameObject _currentPreview;
 
+    private UIDocument _Doc;
+
     // Start is called before the first frame update
     void Start()
     {
+
         _currentPreview = Object.Instantiate(levels[_currentIndex]);
         _currentPreview.AddComponent<RotateObject>();
 
+        _Doc = GetComponent<UIDocument>();
+        Button playButton = _Doc.rootVisualElement.Q<Button>("PlayButton");
+        Button forwardButton = _Doc.rootVisualElement.Q<Button>("ForwardButton");
+        Button backwardButton = _Doc.rootVisualElement.Q<Button>("BackwardButton");
+        playButton.clicked += playButtonOnClicked;
+        forwardButton.clicked += forwardButtonOnClicked;
+        backwardButton.clicked += backwardButtonOnClicked;
     }
 
-    public void nextPreview()
+    private void playButtonOnClicked()
+    {
+        Debug.Log("play Button pressed");
+    }
+    private void forwardButtonOnClicked()
+    {
+        nextPreview();
+    }
+    private void backwardButtonOnClicked()
+    {
+        lastPreview();
+    }
+
+
+     public void nextPreview()
     {
         Destroy(_currentPreview);
         _currentIndex = (_currentIndex + 1) % levels.Length;
