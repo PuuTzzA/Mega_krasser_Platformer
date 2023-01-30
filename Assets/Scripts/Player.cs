239eb027555286f9edfb25b8500643b24cfdd033
myvvv <merrycaa@gmail.com>
2023-01-30 18:23:34 +0100

    nichts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,7 +38,7 @@ public class Player : MonoBehaviour
     private float _groundDashCooldown = 2.0f;
     private int _maxHealth = 3;
     private float _invisFramesDuration = 2.0f;
-    private float _deathDepth;
+    private float _deathDepth ;
 
     public GameObject groundChecker, wallChecker, uiIngameObj, endScreenPrefab;
 
