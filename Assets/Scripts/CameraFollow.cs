924ae21326ac3ef1d3aa19c60382c3eb64b21f7d
Gift69 <gift2lp4k@gmail.com>
2023-02-06 12:43:11 +0100

    fixed camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -35,16 +35,18 @@ public class CameraFollow : MonoBehaviour
         }
         this.transform.SetParent(_rotationPoint.transform);
         this.transform.eulerAngles = new Vector3(0, 180, 0);
-        this.transform.position = new Vector3(0, 0, radius);
+        this.transform.position = new Vector3(0, player.transform.position.y, radius);
 
-        tmp.transform.position = _rotationPoint.transform.position;
-        tmp.transform.localScale = _rotationPoint.transform.localScale;
-        tmp.transform.rotation = _rotationPoint.transform.rotation;
+        _rotationPoint.transform.LookAt(new Vector3(player.transform.position.x, 0, player.transform.position.z));
 
-        tmp.transform.LookAt(new Vector3(player.transform.position.x - player.transform.localScale.x / 4, player.transform.position.y, player.transform.position.z - player.transform.localScale.z / 4));
-        tmp.transform.rotation = new Quaternion(0, tmp.transform.rotation.y, 0, tmp.transform.rotation.w);
+//        tmp.transform.position = _rotationPoint.transform.position;
+//        tmp.transform.localScale = _rotationPoint.transform.localScale;
+//        tmp.transform.rotation = _rotationPoint.transform.rotation;
 
-        _rotationPoint.transform.rotation = Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation, 360);
+//        tmp.transform.LookAt(new Vector3(player.transform.position.x - player.transform.localScale.x / 4, player.transform.position.y, player.transform.position.z - player.transform.localScale.z / 4));
+//        tmp.transform.rotation = new Quaternion(0, tmp.transform.rotation.y, 0, tmp.transform.rotation.w);
+
+//        _rotationPoint.transform.rotation = Quaternion.RotateTowards(_rotationPoint.transform.rotation, tmp.transform.rotation, 360);
     }
     // Update is called once per frame
     void FixedUpdate()
